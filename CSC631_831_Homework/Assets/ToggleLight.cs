ee5b54bc053e739c629b9a0a8ed1d1e8c392df93
Vern Saeteurn <vsaeteurn@mail.sfsu.edu>
2021-02-17 14:53:51 -0800

    Add scenes to branch

---
+++ CSC631_831_Homework/Assets/ToggleLight.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ToggleLight : MonoBehaviour
+{
+    public GameObject directionalLightObject;
+    private Light directionalLight;
+    private int count = 0;
+
+    public void ToggleSunlight()
+    {
+        count++;
+        if(count % 2 == 0)
+        {
+            directionalLight.color = Color.red;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        directionalLight = directionalLightObject.GetComponent<Light>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
