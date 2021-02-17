0f6055f236dd5918f70b9e787f5a57da104f2cc5
Eric Chen <ejc196@gmail.com>
2021-02-17 11:41:01 -0800

    Added button to swap textures on ground object, Added End Trigger to detect Collision

---
+++ CSC631_831_Homework/Assets/Scenes/Player_Movement.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class Player_Movement : MonoBehaviour
+{
+    public Rigidbody rb;
+
+    public float forwardForce = 700f;
+    public float backwardForce = 500f;
+    public float sidewaysForce = 500f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if(Input.GetKey("w")){
+            rb.AddForce(0,0, forwardForce * Time.deltaTime);
+        }
+
+        if(Input.GetKey("s")){
+            rb.AddForce(0,0, -backwardForce * Time.deltaTime);
+        }
+
+        if(Input.GetKey("a")){
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0,0);
+        }
+        
+        if(Input.GetKey("d")){
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0,0);
+        }
+        
+    }
+}
---
+++ CSC631_831_Homework/Assets/ScoreLap.cs
@@ -0,0 +1,13 @@
+﻿
+using UnityEngine;
+
+public class ScoreLap : MonoBehaviour
+{
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
