e51c697f5b3e97380c05dc693434a7eaacb0a971
Eric Chen <ejc196@gmail.com>
2021-02-17 16:44:14 -0800

    Added particle effects to playercube, Scene transition button, sound on trigger

---
+++ CSC631_831_Homework/Assets/Scripts/EndTrigger.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndTrigger : MonoBehaviour
+{
+
+    public AudioClip sound;
+    void Start(){
+        //disable playOnAwake so that it does not play on scene start, but is active for collision play
+        GetComponent<AudioSource>().playOnAwake = false;
+        GetComponent<AudioSource>().clip = sound;
+    }
+
+    public int score = 0;
+    void OnTriggerEnter(){
+        Debug.Log("Entered the Goal");
+        score++;
+        Debug.Log("Current Score: " + score);
+        GetComponent<AudioSource>().Play();
+    }
+}
---
+++ CSC631_831_Homework/Assets/Scripts/ScoreLap.cs
@@ -0,0 +1,17 @@
+﻿
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreLap : MonoBehaviour
+{
+    public Text scoreText;
+
+    public GameObject endScore;
+
+    // Update is called once per frame
+    void Update()
+    {
+        scoreText.text = endScore.GetComponent<EndTrigger>().score.ToString();
+        
+    }
+}
